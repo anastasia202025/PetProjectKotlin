28ae04dee17f1cb1bf046f28e5ef237fb9648dbb
ВАШÐAnastasia <anastasija2003@mail.ru>
2024-01-21 19:48:22 +0300

    Dogs (new project)

---
+++ src/main/kotlin/Dogs.kt
@@ -0,0 +1,20 @@
+class Dog (val name: String,weight_param: Int,breed_param: String){
+    init {
+        print("Dog $name has been created. ")
+    }
+    var activities = arrayOf("Walks")
+    val breed = breed_param.toUpperCase()
+    init {
+        println("The breed is $breed.")
+    }
+    var weight = weight_param
+        set(value){
+            if (value>0) field = value
+        }
+    val weightInKgs : Double
+        get() = weight/2.2
+
+    fun bark(){
+        println(if (weight < 20) "yip!" else "Woof!")
+    }
+}
